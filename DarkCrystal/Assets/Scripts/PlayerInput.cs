b90f54161fba1167d5719a3258d77045fb8941f3
Alexandr <sahsagrom1999@gmail.com>
2020-09-27 21:53:08 +0300

    start to refactor

---
+++ DarkCrystal/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float maxXRotation;
+    [SerializeField] private float maxZRotation;
+
+    private float currentXRotation;
+    private float currentZRotation;
+
+    private void Update()
+    {
+        CalculateXRotation();
+        CalculateZRotation();
+    }
+
+    private void CalculateXRotation()
+    {
+        float rotation = -Input.GetAxis("Vertical") * rotationSpeed * Time.deltaTime;
+        print(Mathf.Abs(transform.localEulerAngles.x + rotation));
+        if (Mathf.Abs(transform.localEulerAngles.x + rotation) <= maxXRotation)
+        {
+            //currentXRotation += rotation;
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + rotation, 0, transform.localEulerAngles.z);
+        }
+    }
+
+    private void CalculateZRotation()
+    {
+        float rotation = -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        if (Mathf.Abs(currentZRotation + rotation) <= maxXRotation)
+        {
+            currentZRotation += rotation;
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 0, currentZRotation);
+        }
+    }
+}
